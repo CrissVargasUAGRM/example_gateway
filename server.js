e77d367ec93273c209f8fb41058c88f965b35010
Cristhian Vargas Quiroz <cristhianingsis@gmail.com>
2023-11-04 10:45:07 -0400

    render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const gateway = require('express-gateway');
 
-console.log(gateway().load(path.join(__dirname, 'config')));
+console.log(gateway().load(path.join(__dirname, 'config', 'gateway.config.yml')));
 
 gateway()
   .load(path.join(__dirname, 'config'))
